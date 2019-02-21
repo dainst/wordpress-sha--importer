ddae0ae8e33338f1c2ed064f7cd26db227601da6
paflov <philipp.franck@dainst.de>
2019-02-21 17:56:17 +0100

    added other required taxonomies

--- functions/keywords.php
+++ functions/keywords.php
@@ -1,10 +1,76 @@
 <?php
+
 add_action('init', function() {
     register_taxonomy(
         'shap_tags',
         'attachment',
         array(
             'label'                 => 'SHAP Tags',
+            'rewrite'               => array('slug' => 'data'),
+            'hierarchical'          => false,
+            'show_ui'               => true,
+            'show_admin_column'     => true,
+            'query_var'             => true,
+        )
+    );
+
+    register_taxonomy(
+        'shap_places',
+        'attachment',
+        array(
+            'label'                 => 'SHAP Places',
+            'rewrite'               => array('slug' => 'data'),
+            'hierarchical'          => false,
+            'show_ui'               => true,
+            'show_admin_column'     => true,
+            'query_var'             => true,
+        )
+    );
+
+    register_taxonomy(
+        'shap_time',
+        'attachment',
+        array(
+            'label'                 => 'SHAP Places',
+            'rewrite'               => array('slug' => 'data'),
+            'hierarchical'          => false,
+            'show_ui'               => true,
+            'show_admin_column'     => true,
+            'query_var'             => true,
+        )
+    );
+
+    register_taxonomy(
+        'shap_theme',
+        'attachment',
+        array(
+            'label'                 => 'SHAP Theme',
+            'rewrite'               => array('slug' => 'data'),
+            'hierarchical'          => false,
+            'show_ui'               => true,
+            'show_admin_column'     => true,
+            'query_var'             => true,
+        )
+    );
+
+    register_taxonomy(
+        'shap_subject',
+        'attachment',
+        array(
+            'label'                 => 'SHAP Subject',
+            'rewrite'               => array('slug' => 'data'),
+            'hierarchical'          => false,
+            'show_ui'               => true,
+            'show_admin_column'     => true,
+            'query_var'             => true,
+        )
+    );
+
+    register_taxonomy(
+        'shap_pool',
+        'attachment',
+        array(
+            'label'                 => 'SHAP Pool',
             'rewrite'               => array('slug' => 'data'),
             'hierarchical'          => false,
             'show_ui'               => true,
